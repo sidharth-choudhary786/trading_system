162126a1acb442b84eb2417362f051794f9760da
Sidharth Choudhary <174858061+sidharth-choudhary786@users.noreply.github.com>
2025-11-09 12:38:13 +0530

    update trading_system

--- trading_system/utils/config.py
+++
@@ -1,9 +0,0 @@
-import yaml
-
-class Config:
-    def __init__(self, config_file):
-        with open(config_file, 'r') as file:
-            self.config = yaml.safe_load(file)
-
-    def get(self, key, default=None):
-        return self.config.get(key, default)
