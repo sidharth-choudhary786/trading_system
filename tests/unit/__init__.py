f1db47782699fd4fc51b641ea956e63cd1901754
Sidharth Choudhary <174858061+sidharth-choudhary786@users.noreply.github.com>
2025-11-09 19:01:00 +0530

    Update __init__.py

--- tests/unit/__init__.py
+++ tests/unit/__init__.py
@@ -2,3 +2,14 @@
 """
 Unit tests for individual components
 """
+
+__all__ = [
+    'test_data_processing',
+    'test_data_sources', 
+    'test_execution',
+    'test_features',
+    'test_models',
+    'test_portfolio',
+    'test_risk',
+    'test_utils'
+]
